e48b9d8e041fcc784a822cd697fae785a9bc4815
Tim Schwenke <tim.and.trallnag+code@gmail.com>
2020-12-03 23:02:35 +0100

    docs: Add missing docstring

--- prometheus_fastapi_instrumentator/instrumentation.py
+++ prometheus_fastapi_instrumentator/instrumentation.py
@@ -198,6 +198,8 @@ class PrometheusFastApiInstrumentator:
 
             endpoint: Endpoint on which metrics should be exposed.
             include_in_schema: Should the endpoint show up in the documentation?
+            tags (List[str], optional): If you manage your routes with tags.
+                Defaults to None. 
 
         Raises:
             ValueError: If `prometheus_multiproc_dir` env var is found but
