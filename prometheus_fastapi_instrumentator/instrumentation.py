bbb606becb79658f5bd99c5e9c860a88b5929ca1
Tim Schwenke <tim.and.trallnag+code@gmail.com>
2020-12-13 19:57:10 +0000

    ci: Apply automatic changes [skip ci]
    Signed-off-by: Tim Schwenke <tim.and.trallnag+code@gmail.com>

--- prometheus_fastapi_instrumentator/instrumentation.py
+++ prometheus_fastapi_instrumentator/instrumentation.py
@@ -223,7 +223,7 @@ class PrometheusFastApiInstrumentator:
             endpoint: Endpoint on which metrics should be exposed.
 
             include_in_schema: Should the endpoint show up in the documentation?
-            
+
             tags (List[str], optional): If you manage your routes with tags.
                 Defaults to None.
 
