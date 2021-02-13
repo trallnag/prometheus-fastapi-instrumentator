1dc0bb331ff2484931030fe802d0ee19a84e13d4
Tim Schwenke <tim.and.trallnag+code@gmail.com>
2021-02-14 00:59:42 +0100

    refactor(dev)!: Switch from underscores to dashes for function names

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ project_name="prometheus_fastapi_instrumentator"
 # ==============================================================================
 # Misc
 
-_docs () {
+function _docs {
     echo "Create docs"
 
     tmp_dir=/tmp/docs
@@ -18,14 +18,14 @@ _docs () {
     rm -rf /tmp/docs
 }
 
-_lint () {
+function _lint {
     echo "Lint project"
 
     poetry run flake8 --config .flake8 --statistics
     poetry run mypy ${project_name} --allow-redefinition
 }
 
-_requirements () {
+function _requirements {
     echo "Create requirements file"
 
     rm -rf "requirements.txt"
@@ -38,19 +38,19 @@ _requirements () {
 # ==============================================================================
 # Format
 
-_format_style () {
+function _format_style {
     echo "Format style"
 
     poetry run black .    
 }
 
-_format_imports () {
+function _format_imports {
     echo "Format imports"
 
     poetry run isort --profile black .
 }
 
-_format () {
+function _format {
     _format_style
     _format_imports
 }
@@ -58,19 +58,19 @@ _format () {
 # ==============================================================================
 # Test
 
-_test_not_slow () {
+function _test_not_slow {
     echo "Run non-slow tests with Pytest"
 
     poetry run pytest -m "not slow" --cov=./ --cov-report=xml
 }
 
-_test_slow () {
+function _test_slow {
     echo "Run slow tests with Pytest"
     
     poetry run pytest -m "slow" --cov-append --cov=./ --cov-report=xml
 }
 
-_test_multiproc () {
+function _test_multiproc {
     mkdir -p /tmp/test_multiproc
     export prometheus_multiproc_dir=/tmp/test_multiproc
     poetry run pytest -k test_multiprocess --cov-append --cov=./ --cov-report=xml
@@ -78,7 +78,7 @@ _test_multiproc () {
     unset prometheus_multiproc_dir
 }
 
-_test () {
+function _test {
     _test_not_slow
     _test_slow
     _test_multiproc
@@ -86,38 +86,40 @@ _test () {
 
 # ==============================================================================
 
-_help () {
+function _help {
     cat << EOF
 Functions you can use like this 'bash run.sh <function name>':
     docs
     lint
     requirements
-    format_style
-    format_imports
+    format-style
+    format-imports
     format
-    test_not_slow
-    test_slow
-    test_multiproc
+    test-not-slow
+    test-slow
+    test-multiproc
     test
 EOF
 }
 
-if [[ $# -eq 0 ]] ; then
+if [[ $# -eq 0 ]]
+then
     _help
 fi
 
 for arg in "$@"
 do
-    if  [ $arg = "help" ]; then _help
+    if  [ $arg = "help" ] || [ $arg = "-help" ] || [ $arg = "--help" ]; then _help
     elif [ $arg = "docs" ]; then _docs
     elif [ $arg = "lint" ]; then _lint
     elif [ $arg = "requirements" ]; then _requirements
-    elif [ $arg = "format_style" ]; then _format_style
-    elif [ $arg = "format_imports" ]; then _format_imports
+    elif [ $arg = "format-style" ]; then _format_style
+    elif [ $arg = "format-imports" ]; then _format_imports
     elif [ $arg = "format" ]; then _format
-    elif [ $arg = "test_not_slow" ]; then _test_not_slow
-    elif [ $arg = "test_slow" ]; then _test_slow
-    elif [ $arg = "test_multiproc" ]; then _test_multiproc
+    elif [ $arg = "test-not-slow" ]; then _test_not_slow
+    elif [ $arg = "test-slow" ]; then _test_slow
+    elif [ $arg = "test-multiproc" ]; then _test_multiproc
     elif [ $arg = "test" ]; then _test
+    else _help
     fi
 done
