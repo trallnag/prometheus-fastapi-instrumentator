1ef3f430993a1aa1bbd271fe4e56a45a6823cde1
Tim Schwenke <tim.schwenke@trallnag.com>
2022-05-01 22:26:47 +0200

    ci: fix coverage report generation

--- scripts/test.sh
+++ scripts/test.sh
@@ -26,24 +26,25 @@ cd "$repo_dir"
 _run_fast_tests() {
     loginfo "Run fast tests with Pytest."
 
-    poetry run pytest -m "not slow" --cov=./ --cov-report=xml
+    poetry run pytest -m "not slow" --cov=./ --cov-report=xml --cov-append
 }
 
 _run_slow_tests() {
     loginfo "Run slow tests with Pytest."
 
-    poetry run pytest -m "slow" --cov=./ --cov-report=xml
+    poetry run pytest -m "slow" --cov=./ --cov-report=xml --cov-append
 }
 
 _run_tests() {
+    rm coverage.xml || true
     _run_fast_tests
     _run_slow_tests
 }
 
 if [ $# -eq 1 ]; then
     case $1 in
-        -f|--fast|fast)     _run_fast_tests ;;
-        -s|--slow|slow)     _run_slow_tests ;;
+        -f|--fast|fast)     (rm coverage.xml || true) && _run_fast_tests ;;
+        -s|--slow|slow)     (rm coverage.xml || true) && _run_slow_tests ;;
         *)                  help
     esac
 else
