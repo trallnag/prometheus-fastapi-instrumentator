5d58e47871417f92aa2ae55d0b598ea8f09e1c7b
Sebastián Ramírez <tiangolo@gmail.com>
2022-06-17 17:14:52 +0200

    ✅ Add tests for custom registry and default registry

--- tests/test_instrumentator_multiple_apps.py
+++ tests/test_instrumentator_multiple_apps.py
@@ -33,13 +33,43 @@ def get_response(client: TestClient, path: str) -> TestClientResponse:
 # Tests
 
 
-def test_expose_defaults():
+def test_expose_defaults_custom_registry():
     app1 = create_app()
     app2 = create_app()
     registry1 = CollectorRegistry(auto_describe=True)
     registry2 = CollectorRegistry(auto_describe=True)
     Instrumentator(registry=registry1).instrument(app1).expose(app1)
     Instrumentator(registry=registry2).instrument(app2).expose(app2)
+
+    # Add middlewares after adding the instrumentator, this triggers another
+    # app.build_middleware_stack(), which creates the middleware again, but it will use
+    # the same Prometheus registry again, which could try to create the same metrics
+    # again causing duplication errors
+    @app1.middleware("http")
+    @app2.middleware("http")
+    async def no_op_middleware(request, call_next):
+        response = await call_next(request)
+        return response
+
+    client1 = TestClient(app1)
+    client2 = TestClient(app2)
+
+    get_response(client1, "/")
+    get_response(client2, "/")
+
+    response1 = get_response(client1, "/metrics")
+    response2 = get_response(client2, "/metrics")
+    assert response1.status_code == 200
+    assert b"http_request" in response1.content
+    assert response2.status_code == 200
+    assert b"http_request" in response2.content
+
+
+def test_expose_defaults():
+    app1 = create_app()
+    app2 = create_app()
+    Instrumentator().instrument(app1).expose(app1)
+    Instrumentator().instrument(app2).expose(app2)
 
     # Add middlewares after adding the instrumentator, this triggers another
     # app.build_middleware_stack(), which creates the middleware again, but it will use
