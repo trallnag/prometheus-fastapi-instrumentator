aa1cfb0ed391e3f023d72e72ca8e713b596682ad
Tim Schwenke <tim@trallnag.com>
2023-03-05 19:43:08 +0100

    test: Adjust tests for multiple apps

--- tests/test_instrumentator_multiple_apps.py
+++ tests/test_instrumentator_multiple_apps.py
@@ -1,95 +1,98 @@
 from fastapi import FastAPI
-from prometheus_client import CollectorRegistry
-from requests import Response as TestClientResponse
+from prometheus_client import CollectorRegistry, Counter
 from starlette.testclient import TestClient
 
 from prometheus_fastapi_instrumentator import Instrumentator
 
-# ------------------------------------------------------------------------------
-# Setup
 
+def test_multiple_apps_custom_registry():
+    app1 = FastAPI()
+    app2 = FastAPI()
 
-def create_app() -> FastAPI:
-    app = FastAPI()
+    @app1.get("/dummy")
+    def read_dummy_app1():
+        return "Hello from app1"
 
-    @app.get("/")
-    def read_root():
-        return "Hello World!"
+    @app2.get("/dummy")
+    def read_dummy_app2():
+        return "Hello from app2"
 
-    return app
-
-
-def get_response(client: TestClient, path: str) -> TestClientResponse:
-    response = client.get(path)
-
-    print(f"\nResponse  path='{path}' status='{response.status_code}':\n")
-    for line in response.content.split(b"\n"):
-        print(line.decode())
-
-    return response
-
-
-# ------------------------------------------------------------------------------
-# Tests
-
-
-def test_expose_defaults_custom_registry():
-    app1 = create_app()
-    app2 = create_app()
     registry1 = CollectorRegistry(auto_describe=True)
     registry2 = CollectorRegistry(auto_describe=True)
+
     Instrumentator(registry=registry1).instrument(app1).expose(app1)
     Instrumentator(registry=registry2).instrument(app2).expose(app2)
 
-    # Add middlewares after adding the instrumentator, this triggers another
-    # app.build_middleware_stack(), which creates the middleware again, but it will use
-    # the same Prometheus registry again, which could try to create the same metrics
-    # again causing duplication errors
+    Counter("test_app1_only", "In app1 metrics only.", registry=registry1).inc()
+
+    # Add middleware after adding the instrumentator, this triggers another
+    # app.build_middleware_stack(), which creates the middleware again, but it
+    # will use the same Prometheus registry again, which could try to create the
+    # same metrics again causing duplication errors.
     @app1.middleware("http")
     @app2.middleware("http")
-    async def no_op_middleware(request, call_next):
+    async def dummy_middleware(request, call_next):
         response = await call_next(request)
         return response
 
     client1 = TestClient(app1)
     client2 = TestClient(app2)
 
-    get_response(client1, "/")
-    get_response(client2, "/")
+    client1.get("/dummy")
+    client2.get("/dummy")
 
-    response1 = get_response(client1, "/metrics")
-    response2 = get_response(client2, "/metrics")
-    assert response1.status_code == 200
-    assert b"http_request" in response1.content
-    assert response2.status_code == 200
-    assert b"http_request" in response2.content
+    metrics1 = client1.get("/metrics").content.decode()
+    metrics2 = client2.get("/metrics").content.decode()
+
+    print("app1 GET /metrics\n" + metrics1)
+    print("app2 GET /metrics\n" + metrics2)
+
+    want = 'http_requests_total{handler="/dummy",method="GET",status="2xx"} 1.0\n'
+    assert want in metrics1
+    assert want in metrics2
+
+    want = "test_app1_only_total 1.0\n"
+    assert want in metrics1
+    assert want not in metrics2
 
 
-def test_expose_defaults():
-    app1 = create_app()
-    app2 = create_app()
+def test_multiple_apps_expose_defaults():
+    app1 = FastAPI()
+    app2 = FastAPI()
+
+    @app1.get("/dummy")
+    def read_dummy_app1():
+        return "Hello from app1"
+
+    @app2.get("/dummy")
+    def read_dummy_app2():
+        return "Hello from app2"
+
     Instrumentator().instrument(app1).expose(app1)
     Instrumentator().instrument(app2).expose(app2)
 
-    # Add middlewares after adding the instrumentator, this triggers another
-    # app.build_middleware_stack(), which creates the middleware again, but it will use
-    # the same Prometheus registry again, which could try to create the same metrics
-    # again causing duplication errors
+    # Add middleware after adding the instrumentator, this triggers another
+    # app.build_middleware_stack(), which creates the middleware again, but it
+    # will use the same Prometheus registry again, which could try to create the
+    # same metrics again causing duplication errors.
     @app1.middleware("http")
     @app2.middleware("http")
-    async def no_op_middleware(request, call_next):
+    async def dummy_middleware(request, call_next):
         response = await call_next(request)
         return response
 
     client1 = TestClient(app1)
     client2 = TestClient(app2)
 
-    get_response(client1, "/")
-    get_response(client2, "/")
+    client1.get("/dummy")
+    client2.get("/dummy")
 
-    response1 = get_response(client1, "/metrics")
-    response2 = get_response(client2, "/metrics")
-    assert response1.status_code == 200
-    assert b"http_request" in response1.content
-    assert response2.status_code == 200
-    assert b"http_request" in response2.content
+    metrics1 = client1.get("/metrics").content.decode()
+    metrics2 = client2.get("/metrics").content.decode()
+
+    print("app1 GET /metrics\n" + metrics1)
+    print("app2 GET /metrics\n" + metrics2)
+
+    want = 'http_requests_total{handler="/dummy",method="GET",status="2xx"} 1.0\n'
+    assert want in metrics1
+    assert want in metrics2
